5a0a9cae03d837515e75e2452c22327de9d81bdd
Jojo4511 <97646229+Jojo4511@users.noreply.github.com>
2022-05-04 21:49:06 -0700

    i added stuff

--- Skill.java
+++ Skill.java
@@ -1,6 +1,14 @@
 public class Skill
 {
-  public Skill()
+  String name;
+  public Skill(String name)
   {
+    this.name = name;
   }
+
+  public String Greeting() 
+  {
+    return "Why hello " + name;
+  }
+
 }
--- SkillTest.java
+++ SkillTest.java
@@ -1,4 +1,12 @@
+import static org.junit.Assert.*; 
+import org.junit.*; 
+
 public class SkillTest
 {
-  
+  @Test
+  public void greetingTest()
+  {
+      Skill skill = new Skill("Johannes");
+      assertEquals("Why hello Onat", skill.Greeting());
+  }
 }
